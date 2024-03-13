5eccd9f5d294d3244198d7af588b108c9bb75c5f
carlossiq <31148477+carlossiq@users.noreply.github.com>
2024-03-13 11:41:04 -0300

    up

--- MVE.cpp
+++ MVE.cpp
@@ -1,3 +1,17 @@
+/*
+Confeccione um sistema de matrículas em disciplinas de uma faculdade:
+Entidades mais importantes: Aluno e Disciplina
+Aluno: código (5 dígitos), nome, CPF
+Disciplina: código (4 dígitos), nome, professor, quatidade de créditos
+O sistema deve ser capaz de armazenar uma lista de alunos e uma lista de disciplinas por periodo (2018.2)
+Estruturas alocadas dinamicamente
+Opções: Inserção, remoção (através de um menu de opções)
+        Consultas das disciplinas de um aluno em um determinado periodo
+        Consultas dos alunos que estão matriculados em uma disciplina em um determinado periodo
+        Salvamento e recuperação dos dados entre sessão
+Entregar arquivo (mínimo 3 alunos, disciplinas e alocações)
+*/
+
 #include <iostream>
 
 int main()
